a0fe7cd833e8e31ad0bc93b6e6286decdc08ae27
BryanIRP <bryanimanolrp@gmail.com>
2023-09-17 18:00:49 +0200

    commit-google search console

--- aside.php
+++
@@ -1,4 +0,0 @@
-<aside>
-	<h3>AURW Pro coming soon!</h3><br>
-	<p>ABKaspo Development team is working for AURW Pro, the best water? idk but in my opinion is coming a good package. When? idk we are just starting with FFT and Ocean Spectra and a real good shader.</p>
-</aside>
